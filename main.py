60c205c1ca13e4a8d57c1ab26780aceded037535
TethirEntwistle <tyashdavis@gmail.com>
2024-08-23 22:08:38 -0500

    initial game loop

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-print("testing")
 import pygame # type: ignore
 from constants import *
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+       for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+       
+       screen.fill((0, 0, 0))
+       pygame.display.flip()
+       
+
 
 
 if __name__ == "__main__":
