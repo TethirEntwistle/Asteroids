23243f11e83760a3a8aefc7aa49b75f881f6da0c
TethirEntwistle <tyashdavis@gmail.com>
2024-08-23 19:14:56 -0500

    modules added

---
+++ main.py
@@ -0,0 +1,16 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+print("testing")
+import pygame # type: ignore
+from constants import *
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
