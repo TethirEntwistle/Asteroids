9db27a33f1548beda7a7c117a87aa988f4b08c11
TethirEntwistle <tyashdavis@gmail.com>
2024-08-27 13:59:46 -0500

    Chapter 2 Done

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+# use these to get into the virtual machine
+# python3 -m venv venv
+# source venv/bin/activate
 import pygame # type: ignore
 from constants import *
+from player import Player
+
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsClock = pygame.time.Clock()
+    dt = 0
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    mainPlayer = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
        
+       for object in updatable:
+           object.update(dt)
        screen.fill("black")
+       for object in drawable:
+           object.draw(screen)
+       
        pygame.display.flip()
+       
+       dt = fpsClock.tick(60) / 1000
+       
+    
+    
        
 
 
